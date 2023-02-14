d3daff7ae6d017f0f5805ad13ee39782d65e126f
Voxaire <voxairegaming16@gmail.com>
2023-02-14 15:17:42 -0500

    More Exampe

--- Basics/src/Basics/Functions.java
+++ Basics/src/Basics/Functions.java
@@ -13,12 +13,23 @@ public class Functions {
         return celTemp;
     }
 
+
+    public static float getCost(float cost1, float cost2) { //Will return a float.
+        float avgCost=  (cost1 + cost2) / 2;
+        return avgCost;
+    }
     public static void main(String[] args) {
         System.out.print("What is your name?: ");
-        String name = getName();
+        String name = getName(); //Call the function as a variable
         System.out.println(name);
         System.out.print("Enter a temperature in Fahrenheit: ");
         float temp = getTemp();
         System.out.println(temp);
+        System.out.print("Enter the cost of item: ");
+        float cost1 = scnr.nextFloat();
+        System.out.print("Enter the cost of another item: ");
+        float cost2 = scnr.nextFloat();
+        float avgCost = getCost(cost1,cost2); //Transfers variables from one method to another.
+        System.out.println("The Average cost of both items is $" + avgCost);
     }
 }
