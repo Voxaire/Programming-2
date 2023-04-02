322330c0a778103f041d517a79f4968a296f0798
Voxaire <voxairegaming16@gmail.com>
2023-04-02 16:16:16 -0400

    AS5

--- Basics/src/AS5/Q1.java
+++ Basics/src/AS5/Q1.java
@@ -2,22 +2,16 @@ package AS5;
 abstract class Pen {
     abstract void write();
     abstract void refill();
-
-
 }
 class fountainPen extends Pen {
-
     @Override
     void write() {
         System.out.println("Write");
     }
-
     @Override
     void refill() {
         System.out.println("Refill");
-
     }
-
     void changeNib(){
         System.out.println("Changing the Nib");
     }
--- Basics/src/AS5/Q2.java
+++ Basics/src/AS5/Q2.java
@@ -4,34 +4,32 @@ interface basicanimal{
     void eat();
     void sleep();
 }
-
 class monkey{
     void jump() {
-
+        System.out.println("Jumping");
     }
-
     void bite(){
+        System.out.println("Biting");
     }
 }
-class Human extends monkey implements basicanimal{
-    public void eat(){
+class Human extends monkey implements basicanimal {
+    @Override
+    public void eat() {
         System.out.println("Eating");
-
     }
     @Override
     public void sleep() {
         System.out.println("Sleeping");
-
-    }
-    public void speak() {
-        System.out.println("Hello!!");
     }
 }
 public class Q2 {
-    public static void main(String[] args) {
-        Human human = new Human();
-        human.speak();
-        human.eat();
-        human.bite();
+        public static void main(String[] args) {
+            Human human = new Human();
+            monkey monkey = new monkey();
+            human.eat();
+            human.bite();
+            monkey.bite();
+            monkey.jump();
+        }
     }
-}
+
